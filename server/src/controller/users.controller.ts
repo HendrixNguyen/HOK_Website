bc710e7e20bcff6d2d9d4903ffd4f6028138ae9d
Huyndg <huyndg.levixdev@gmail.com>
2021-10-01 21:52:01 +0700

    Xem thu dung huong chua

---
+++ server/src/controller/users.controller.ts
@@ -0,0 +1,76 @@
+import { User } from '../entities/User'
+import { NextFunction, Request, Response, Router } from 'express'
+import { getConnection } from 'typeorm'
+import dbConnection from 'src/config/dbconfig'
+
+export class UserController {
+  public router: Router
+
+  constructor() {
+    this.router = Router()
+    this.routes()
+  }
+
+  public getAllUser = async (
+    _req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const users = await User.find()
+      return res.status(201).json({ users })
+    } catch (err) {
+      return await res.status(404).send(err)
+    }
+  }
+
+  public createUser = async (
+    req: Request,
+    res: Response
+  ): Promise<Response> => {
+    try {
+      const newUser = req.body()
+      // const { } = req.body();
+      if (newUser.username === User.findOne('username')) {
+        const result = await User.save(newUser)
+        return res.json(result)
+      }
+      return res.status(200).send('Succeed')
+    } catch (err) {
+      return res.status(400).send('Error creating User')
+    }
+  }
+
+  //find 1 user:id
+  public findUser = async (
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ) => {
+    try {
+      res.status(201).send('succeed')
+    } catch (err) {
+      res.status(404).send(err)
+    }
+  }
+
+  //update user
+  public updateUser = async (
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ) => {
+    try {
+      res.status(201).send('succeed')
+    } catch (err) {
+      res.status(404).send(err)
+    }
+  }
+  //delete user
+
+  public routes() {
+    this.router.get('/', this.getAllUser)
+    this.router.post('/signup', this.createUser)
+    this.router.post('/login', this.findUser)
+    this.router.put('/:id', this.updateUser)
+  }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express'
+import morgan from 'morgan'
 import dbConnection from './config/dbconfig'
-
-import { UserController } from './controller/index'
+import { UserController } from './controller/users.controller'
 
 export class Server {
   private app: Application
@@ -15,7 +15,7 @@ export class Server {
   }
 
   public async routes() {
-    await dbConnection()
+    dbConnection()
     this.UserController = new UserController()
 
     this.app.get('/auth/', this.UserController.router)
@@ -26,11 +26,13 @@ export class Server {
 
   public configuration(): void {
     this.app.set('port', process.env.PORT || 3000)
+    this.app.use(express.json())
+    this.app.use(morgan('dev'))
   }
 
   public start() {
     this.app.listen(this.app.get('port'), () => {
-      console.log(`This server has been started on ${this.app.get('port')})}`)
+      console.log(`This server has been started on ${this.app.get('port')}`)
     })
   }
 }
