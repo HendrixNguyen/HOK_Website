1939a36ecc0c09903d80448ee0b703cb4a35ced6
Huyndg <huyndg.levixdev@gmail.com>
2021-10-01 15:14:29 +0700

    change main func to class

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
 import express, { Application, Request, Response } from 'express'
+import dbConnection from './config/dbconfig'
+
 import { UserController } from './controller/index'
 
 export class Server {
@@ -12,12 +14,16 @@ export class Server {
     this.routes()
   }
 
-  public routes() {
+  public async routes() {
+    await dbConnection()
+    this.UserController = new UserController()
+
     this.app.get('/auth/', this.UserController.router)
-    this.app.get('/', async (req: Request, res: Response) => {
+    this.app.get('/', async (_req: Request, res: Response) => {
       await res.status(200).json({ message: 'This is default home' })
     })
   }
+
   public configuration(): void {
     this.app.set('port', process.env.PORT || 3000)
   }
