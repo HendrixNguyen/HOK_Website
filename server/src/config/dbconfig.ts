0c032b77eaa0cf394ffd76ab2858fe52c65b515f
Nguyễn Đình Gia Huy <48069618+Levinguyen11@users.noreply.github.com>
2021-10-15 20:05:29 +0700

    change DB to MySQL (#6)
    Co-authored-by: Huyndg <huyndg.levixdev@gmail.com>

--- server/src/config/dbconfig.ts
+++ server/src/config/dbconfig.ts
@@ -7,7 +7,11 @@ const dbConnection = async () => {
   try {
     const conn = await createConnection({
       type: 'postgres',
-      url: process.env.DB_DEV_URI,
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_USERNAME_DEV,
+      password: process.env.DB_PASSWORD_DEV,
+      database: process.env.DB_DATABASE_DEV,
       synchronize: true,
       logging: true,
       entities: ['src/entity/*.ts'],
