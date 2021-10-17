6a799be0cb8a8208f51f5d8d3c3449189463eecd
Huyndg <huyndg.levixdev@gmail.com>
2021-10-17 15:00:51 +0700

    Exit if can't connect to DATABASE

--- server/src/config/dbconfig.ts
+++ server/src/config/dbconfig.ts
@@ -6,16 +6,20 @@ dotenv.config()
 const dbConnection = async () => {
   try {
     const conn = await createConnection({
-      type: 'postgres',
-      host: 'localhost',
-      port: 3306,
-      username: process.env.DB_USERNAME_DEV,
-      password: process.env.DB_PASSWORD_DEV,
-      database: process.env.DB_DATABASE_DEV,
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || 3306),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       synchronize: true,
       logging: true,
       entities: ['src/entity/*.ts'],
       migrations: ['src/migrations/**/*.ts'],
+    }).catch((e) => {
+      console.error('ERROR: Không thể kết nối tới Database !')
+      console.error(e)
+      process.exit(1)
     })
 
     console.log(`DB is connected to ${conn.isConnected}`)
